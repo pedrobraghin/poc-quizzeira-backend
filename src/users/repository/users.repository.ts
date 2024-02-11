8c379792de510bf214c3affb903ade165934bb2c
Pedro Braghin <braghindev@gmail.com>
2024-02-11 16:35:59 -0300

    feat: add function to update user by provider id

--- src/users/repository/users.repository.ts
+++ src/users/repository/users.repository.ts
@@ -37,6 +37,17 @@ export class UsersRepository {
     return user;
   }
 
+  async updateByProviderId(providerID: string, userDto: UpdateUserDTO) {
+    const user = await this.prismaService.user.update({
+      where: {
+        providerID,
+      },
+      data: userDto,
+    });
+
+    return user;
+  }
+
   async create(userDto: CreateUserDTO) {
     const user = await this.prismaService.user.create({
       data: userDto,
