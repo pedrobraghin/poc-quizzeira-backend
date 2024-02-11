9e71ae8622248697ef96ab4c71f22d6481a5a1c8
Pedro Braghin <braghindev@gmail.com>
2024-02-11 16:32:21 -0300

    feat: add logger decorator

---
+++ src/common/logger.decorator.ts
@@ -0,0 +1,14 @@
+import { Injectable, NestMiddleware } from '@nestjs/common';
+import { Request, Response, NextFunction } from 'express';
+
+@Injectable()
+export class LoggerMiddleware implements NestMiddleware {
+  use(req: Request, _res: Response, next: NextFunction) {
+    console.log(`[${req.method}]: `, req.originalUrl);
+    console.log({
+      cookies: req.cookies,
+      body: req.body,
+    });
+    next();
+  }
+}
