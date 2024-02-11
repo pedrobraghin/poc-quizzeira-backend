a01aa810b1edf762bf255696f491a17ee4a72cc7
Pedro Braghin <braghindev@gmail.com>
2024-02-11 16:35:43 -0300

    feat: add user dto

--- src/users/dtos/user.dto.ts
+++ src/users/dtos/user.dto.ts
@@ -1,1 +1,11 @@
-export interface UserDTO {}
+export interface UserDTO {
+  id: string;
+  provider: string;
+  providerID: string;
+  picture: string;
+  name: string;
+  email: string;
+  passwordHash: string;
+  createdAt: Date;
+  updatedAt: Date;
+}
