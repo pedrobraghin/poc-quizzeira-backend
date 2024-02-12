ada9e9d6811d56a84fecf177a4ef8443c1a5c820
Pedro Braghin <braghindev@gmail.com>
2024-02-12 14:53:01 -0300

    feat: add validation to quizzes dtos

--- src/quizzes/dtos/request/answer-quizz-req.dto.ts
+++ src/quizzes/dtos/request/answer-quizz-req.dto.ts
@@ -1,6 +1,21 @@
+import {
+  ArrayMinSize,
+  IsArray,
+  IsNotEmpty,
+  IsString,
+  ValidateNested,
+} from 'class-validator';
 import { CreateAnswerReqDTO } from './create-answer-req.dto';
+import { Type } from 'class-transformer';
 
 export class AnswerQuizzReqDTO {
+  @IsString()
+  @IsNotEmpty()
   quizzId: string;
+
+  @IsArray()
+  @ArrayMinSize(1)
+  @Type(() => CreateAnswerReqDTO)
+  @ValidateNested({ each: true })
   answers: Array<CreateAnswerReqDTO>;
 }
--- src/quizzes/dtos/request/create-quizz-req.dto.ts
+++ src/quizzes/dtos/request/create-quizz-req.dto.ts
@@ -1,9 +1,22 @@
+import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 import { CreateQuestionReqDTO } from 'src/question/dtos';
 
 export class CreateQuizzReqDTO {
   questions: Array<CreateQuestionReqDTO>;
+  @IsString()
+  @IsNotEmpty()
   title: string;
+
+  @IsString()
+  @IsNotEmpty()
   description: string;
+
+  @IsString()
+  @IsNotEmpty()
   category: string;
+
+  @IsOptional()
+  @IsString()
+  @IsNotEmpty()
   image?: string;
 }
